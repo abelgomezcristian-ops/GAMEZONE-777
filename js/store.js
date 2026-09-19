2c9e53e425d6794bc854f70d128cae29eab83b0f
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 01:23:11 -0300

    Create store.js

---
+++ js/store.js
@@ -0,0 +1,55 @@
+// PS2 - 4 juegos migrados de tu script.js comprobado
+const PS2_GAMES = [
+  {
+    id: 1,
+    titulo: "GTA San Andreas",
+    consola: "ps2",
+    genero: "Mundo abierto",
+    precio: 2000,
+    oldPrice: 2500,
+    estado: "Oferta",
+    featured: true,
+    descripcion: "Clásico eterno de mundo abierto",
+    img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2lbw.jpg",
+    imgLocal: "img/ps2/mundo-abierto/gta-san-andreas.webp",
+    oferta: true
+  },
+  {
+    id: 2,
+    titulo: "God of War II",
+    consola: "ps2",
+    genero: "Hack & Slash",
+    precio: 2500,
+    oldPrice: 3000,
+    estado: "Destacado",
+    featured: true,
+    descripcion: "La ira de Kratos continúa",
+    img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg",
+    imgLocal: "img/ps2/hack-and-slash/god-of-war-2.webp",
+    oferta: true
+  },
+  {
+    id: 3,
+    titulo: "Resident Evil 4",
+    consola: "ps2",
+    genero: "Terror",
+    precio: 2000,
+    estado: "Disponible",
+    featured: false,
+    descripcion: "Survival horror que marcó una era",
+    img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg",
+    imgLocal: "img/ps2/terror/resident-evil-4.webp"
+  },
+  {
+    id: 4,
+    titulo: "PES 2013",
+    consola: "ps2",
+    genero: "Deportes",
+    precio: 2000,
+    estado: "Disponible",
+    featured: false,
+    descripcion: "Fútbol clásico",
+    img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2wzn.jpg",
+    imgLocal: "img/ps2/deportes/pes-2013.webp"
+  }
+];
