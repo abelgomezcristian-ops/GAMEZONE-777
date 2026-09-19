56b67440f0f18fcd4c0b1a33b3e966ad54a81739
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 19:53:26 -0300

    Update mundo-abierto.js

--- data/ps2/mundo-abierto.js
+++ data/ps2/mundo-abierto.js
@@ -1,6 +1,2 @@
-GZ_JUEGOS.push(
-  { id: 1, titulo: "GTA San Andreas", consola: "ps2", genero: "Mundo abierto", precio: 2000, oldPrice: 2500, estado: "Oferta", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2lbw.jpg", imgLocal: "img/ps2/mundo-abierto/gta-san-andreas.webp" },
-  { id: 12, titulo: "GTA Vice City", consola: "ps2", genero: "Mundo abierto", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2lbw.jpg", imgLocal: "img/ps2/mundo-abierto/gta-vice-city.webp" },
-  { id: 13, titulo: "Bully", consola: "ps2", genero: "Mundo abierto", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/mundo-abierto/bully.webp" },
-  { id: 14, titulo: "Scarface", consola: "ps2", genero: "Mundo abierto", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/mundo-abierto/scarface.webp" }
-);
+window.GZ_JUEGOS = window.GZ_JUEGOS || [];
+// pendiente agregar juegos - no rompe
