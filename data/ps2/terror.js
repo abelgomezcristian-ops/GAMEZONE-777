d01627915240b7e6528f341a46c30627ba43a6c2
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 20:05:17 -0300

    Update terror.js

--- data/ps2/terror.js
+++ data/ps2/terror.js
@@ -1,5 +1,2 @@
-GZ_JUEGOS.push(
-  { id: 3, titulo: "Resident Evil 4", consola: "ps2", genero: "Terror", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/terror/resident-evil-4.webp" },
-  { id: 18, titulo: "Silent Hill 2", consola: "ps2", genero: "Terror", precio: 2500, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/terror/silent-hill-2.webp" },
-  { id: 19, titulo: "Silent Hill 3", consola: "ps2", genero: "Terror", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/terror/silent-hill-3.webp" }
-);
+window.GZ_JUEGOS = window.GZ_JUEGOS || [];
+// pendiente agregar juegos - no rompe
