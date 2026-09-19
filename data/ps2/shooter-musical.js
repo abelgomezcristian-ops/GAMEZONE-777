59df191e7b6641ff4680efc6a7201c2d453e59b3
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 20:01:53 -0300

    Update and rename data/ps2/shooter-musical.js to simulacion.js

--- data/ps2/shooter-musical.js
+++
@@ -1,4 +0,0 @@
-GZ_JUEGOS.push(
-  { id: 30, titulo: "Black", consola: "ps2", genero: "Shooter", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/shooter/black.webp" },
-  { id: 31, titulo: "Guitar Hero 2", consola: "ps2", genero: "Musical", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/musical/guitar-hero-2.webp" }
-);
