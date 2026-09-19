4ff32712654068ac81abe35197b71ad945f8ca71
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 02:45:46 -0300

    Create pelea.js

---
+++ data/ps2/pelea.js
@@ -0,0 +1,6 @@
+GZ_JUEGOS.push(
+  { id: 23, titulo: "Dragon Ball Z Budokai Tenkaichi 3", consola: "ps2", genero: "Pelea", precio: 2500, estado: "Destacado", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg", imgLocal: "img/ps2/pelea/dbz-bt3.webp" },
+  { id: 24, titulo: "Tekken 5", consola: "ps2", genero: "Pelea", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg", imgLocal: "img/ps2/pelea/tekken-5.webp" },
+  { id: 25, titulo: "Mortal Kombat Shaolin Monks", consola: "ps2", genero: "Pelea", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg", imgLocal: "img/ps2/pelea/mk-shaolin.webp" },
+  { id: 32, titulo: "Def Jam Fight for NY", consola: "ps2", genero: "Pelea", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg", imgLocal: "img/ps2/pelea/def-jam.webp" }
+);
