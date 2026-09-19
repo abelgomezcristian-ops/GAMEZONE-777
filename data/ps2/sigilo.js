73b187889c3f892289d36cbf825262171f30b321
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 20:04:33 -0300

    Delete data/ps2/sigilo.js

--- data/ps2/sigilo.js
+++
@@ -1,3 +0,0 @@
-GZ_JUEGOS.push(
-  { id: 26, titulo: "Metal Gear Solid 3 Snake Eater", consola: "ps2", genero: "Sigilo", precio: 2500, estado: "Destacado", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/sigilo/mgs3.webp" }
-);
