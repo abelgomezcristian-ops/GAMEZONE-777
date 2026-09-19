d10d41c09a09ef2dd80367c68255a7922e881b61
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 02:44:20 -0300

    Create carreras.js

---
+++ data/ps2/carreras.js
@@ -0,0 +1,4 @@
+GZ_JUEGOS.push(
+  { id: 21, titulo: "Gran Turismo 4", consola: "ps2", genero: "Carreras", precio: 2500, estado: "Destacado", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2wzn.jpg", imgLocal: "img/ps2/carreras/gran-turismo-4.webp" },
+  { id: 22, titulo: "Need for Speed Most Wanted", consola: "ps2", genero: "Carreras", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2wzn.jpg", imgLocal: "img/ps2/carreras/nfs-most-wanted.webp" }
+);
