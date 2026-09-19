73923a5fe2900761254a61888fa41db47fe18aeb
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 19:57:42 -0300

    Update and rename data/ps2/hack-and-slash.js to data/accion.js

--- data/ps2/hack-and-slash.js
+++
@@ -1,5 +0,0 @@
-GZ_JUEGOS.push(
-  { id: 2, titulo: "God of War II", consola: "ps2", genero: "Hack & Slash", precio: 2500, oldPrice: 3000, estado: "Destacado", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg", imgLocal: "img/ps2/hack-and-slash/god-of-war-2.webp" },
-  { id: 15, titulo: "God of War", consola: "ps2", genero: "Hack & Slash", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg", imgLocal: "img/ps2/hack-and-slash/god-of-war-1.webp" },
-  { id: 16, titulo: "Devil May Cry 3", consola: "ps2", genero: "Hack & Slash", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6o.jpg", imgLocal: "img/ps2/hack-and-slash/dmc3.webp" }
-);
