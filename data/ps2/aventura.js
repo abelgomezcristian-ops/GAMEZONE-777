c035f6ad4c1d376c53cafbfa2d7eb24e30f30de5
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 02:40:13 -0300

    Create aventura.js

---
+++ data/ps2/aventura.js
@@ -0,0 +1,3 @@
+GZ_JUEGOS.push(
+  { id: 17, titulo: "Shadow of the Colossus", consola: "ps2", genero: "Aventura", precio: 2500, estado: "Destacado", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/aventura/shadow-colossus.webp" }
+);
