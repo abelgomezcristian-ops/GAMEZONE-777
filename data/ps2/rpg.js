143c4c957f22cb832b15733eee255864928adbf4
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 02:49:56 -0300

    Create rpg.js

---
+++ data/ps2/rpg.js
@@ -0,0 +1,5 @@
+GZ_JUEGOS.push(
+  { id: 27, titulo: "Kingdom Hearts 2", consola: "ps2", genero: "RPG", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/rpg/kingdom-hearts-2.webp" },
+  { id: 28, titulo: "Final Fantasy X", consola: "ps2", genero: "RPG", precio: 2500, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/rpg/ffx.webp" },
+  { id: 29, titulo: "Persona 4", consola: "ps2", genero: "RPG", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1ur5.jpg", imgLocal: "img/ps2/rpg/persona-4.webp" }
+);
