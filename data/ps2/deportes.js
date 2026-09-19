01895ca35b0e74e3e0c577d07450994318e8595c
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-19 02:42:55 -0300

    Create deportes.js

---
+++ data/ps2/deportes.js
@@ -0,0 +1,4 @@
+GZ_JUEGOS.push(
+  { id: 4, titulo: "PES 2013", consola: "ps2", genero: "Deportes", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2wzn.jpg", imgLocal: "img/ps2/deportes/pes-2013.webp" },
+  { id: 20, titulo: "FIFA 14", consola: "ps2", genero: "Deportes", precio: 2000, estado: "Disponible", img: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2wzn.jpg", imgLocal: "img/ps2/deportes/fifa-14.webp" }
+);
