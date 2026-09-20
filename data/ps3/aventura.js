40a9bd8e0f10ecb5c62db0b87719d6e57c4d34aa
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-20 01:18:49 -0300

    Update aventura.js

--- data/ps3/aventura.js
+++ data/ps3/aventura.js
@@ -1,4 +1,4 @@
 window.GZ_JUEGOS = window.GZ_JUEGOS || [];
 GZ_JUEGOS.push(
-  { id: 101, titulo: "Uncharted 2", consola: "ps3", genero: "Aventura", precio: 3500, estado: "Destacado", imagen: "img/ps3/aventura/uncharted-2.webp" }
+  { id: 101, titulo: "Uncharted 2", consola: "ps3", genero: "Aventura", precio: 3500, estado: "Destacado", img: "img/ps3/aventura/uncharted-2.webp", imgLocal: "img/ps3/aventura/uncharted-2.webp" }
 );
