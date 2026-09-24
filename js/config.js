00ba6119f360b2d52c6522b92961927fa5c63bb6
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-24 00:52:17 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const GZ_CONFIG = {
-  whatsapp: "5493795118415",
-  precios: { ps2: { normal: 2000, oferta: 1500, destacado: 2500 }, ps3: { normal: 3000, oferta: 2500 }, switch: { normal: 3000, oferta: 2500 } },
-  cargaInicial: 24,
-  consolas: { ps2:{activa:true}, ps3:{activa:true}, switch:{activa:true}, ps4:{activa:false,label:"PRÓXIMAMENTE"}, ps5:{activa:false,label:"PRÓXIMAMENTE"} }
-};
